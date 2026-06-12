5aec5d89ee2e0b3a388f2f6509706352bd4b05bc
GrachevaJ <gra.che.va@yandex.ru>
2026-06-12 18:38:31 +0700

    Добавляет страницы приложения

---
+++ project/src/pages/not-found/not-found.tsx
@@ -0,0 +1,17 @@
+const NotFound = ():JSX.Element => (
+  <main className="decorated-page quest-page">
+    <div className="decorated-page__decor" aria-hidden="true">
+      <picture>
+        <source type="image/webp" srcSet="img/content/maniac/maniac-size-m.webp, img/content/maniac/maniac-size-m@2x.webp 2x" /><img src="img/content/maniac/maniac-size-m.jpg" srcSet="img/content/maniac/maniac-size-m@2x.jpg 2x" width="1366" height="768" alt="" />
+      </picture>
+    </div>
+    <div className="container container--size-l">
+      <div className="quest-page__content">
+        <h1 className="title title--size-l title--uppercase quest-page__title">404<br/>страница не найдена</h1>
+        <a className="btn btn--accent btn--cta quest-page__btn" href="booking.html">на главную</a>
+      </div>
+    </div>
+  </main>
+);
+
+export default NotFound;
