af2ab60115ba6f1fbd19b097cd216c93df34b08a
GrachevaJ <gra.che.va@yandex.ru>
2026-06-15 14:25:01 +0700

    Исправляет отображение Header на основе признака авторизации

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,35 +1,44 @@
-import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { Link, useLocation } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks/use-app';
 
-const Header = ():JSX.Element => (
-  <header className="header">
-    <div className="container container--size-l">
-      <Link to={AppRoute.Catalog}>
-        <span className="logo header__logo">
-          <svg width="134" height="52" aria-hidden="true">
-            <use xlinkHref="#logo"></use>
-          </svg>
-        </span>
-      </Link>
-      <nav className="main-nav header__main-nav">
-        <ul className="main-nav__list">
-          <li className="main-nav__item">
-            <Link className="link " to={AppRoute.Catalog}>Квесты</Link>
-          </li>
-          <li className="main-nav__item">
-            <Link className="link" to={AppRoute.Contacts}>Контакты</Link>
-          </li>
-          <li className="main-nav__item">
-            <Link className="link" to={AppRoute.MyQuests}>Мои бронирования</Link>
-          </li>
-        </ul>
-      </nav>
-      <div className="header__side-nav">
-        <a className="btn btn--accent header__side-item" href="#">Выйти</a>
-        <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
+const Header = ():JSX.Element => {
+  const authorzationStatus = useAppSelector((state) => state.authorizationStatus);
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <div className="container container--size-l">
+        <Link to={AppRoute.Catalog}>
+          <span className="logo header__logo">
+            <svg width="134" height="52" aria-hidden="true">
+              <use xlinkHref="#logo"></use>
+            </svg>
+          </span>
+        </Link>
+        <nav className="main-nav header__main-nav">
+          <ul className="main-nav__list">
+            <li className="main-nav__item">
+              <Link className={`link ${location.pathname === AppRoute.Catalog ? 'active' : ''}`} to={AppRoute.Catalog}>Квесты</Link>
+            </li>
+            <li className="main-nav__item">
+              <Link className={`link ${location.pathname === AppRoute.Contacts ? 'active' : ''}`} to={AppRoute.Contacts}>Контакты</Link>
+            </li>
+            {authorzationStatus === AuthorizationStatus.Auth ? (
+              <li className="main-nav__item">
+                <Link className={`link ${location.pathname === AppRoute.MyQuests ? 'active' : ''}`} to={AppRoute.MyQuests}>Мои бронирования</Link>
+              </li>
+            ) : ''}
+          </ul>
+        </nav>
+        <div className="header__side-nav">
+          {location.pathname === '/login' ? '' : (
+            <a className="btn btn--accent header__side-item" href="#">{authorzationStatus === AuthorizationStatus.Auth ? 'Выйти' : 'Войти'}</a>
+          )}
+          <a className="link header__side-item header__phone-link" href="tel:88003335599">8 (000) 111-11-11</a>
+        </div>
       </div>
-    </div>
-  </header>
-);
-
+    </header>
+  );
+};
 export default Header;
