6b8addf049b6e3aca7ac5f4ce4126ae0243215c6
GrachevaJ <gra.che.va@yandex.ru>
2026-06-17 13:34:30 +0700

    Добавляет валидацию на страницу входа

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,19 +1,22 @@
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/use-app';
-import { FormEvent } from 'react';
 import { loginUser } from '../../store/actions';
 import { UserAuth } from '../../types/types';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const Login = ():JSX.Element => {
   const dispatch = useAppDispatch();
+  const [loginError, setLoginError] = useState<string | null>(null);
 
-  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    const formData = new FormData(form) as Iterable<[UserAuth]>;
-    const data = Object.fromEntries(formData);
+  const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm<UserAuth & {'user-agreement': boolean}>();
 
-    dispatch(loginUser(data));
+  const onSubmit = async (data: UserAuth & {'user-agreement': boolean}) => {
+    try {
+      await dispatch(loginUser(data)).unwrap();
+    } catch (error) {
+      setLoginError('Ошибка авторизации');
+    }
   };
 
 
@@ -26,23 +29,50 @@ const Login = ():JSX.Element => {
       </div>
       <div className="container container--size-l">
         <div className="login__form">
-          <form className="login-form" action="https://echo.htmlacademy.ru/" method="post" onSubmit={handleFormSubmit}>
+          <form className="login-form" action="https://echo.htmlacademy.ru/" method="post" onSubmit={(evt) => void handleSubmit(onSubmit)(evt)}>
             <div className="login-form__inner-wrapper">
               <h1 className="title title--size-s login-form__title">Вход</h1>
               <div className="login-form__inputs">
                 <div className="custom-input login-form__input">
                   <label className="custom-input__label" htmlFor="email">E&nbsp;&ndash;&nbsp;mail</label>
-                  <input type="email" id="email" name="email" placeholder="Адрес электронной почты" required />
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Адрес электронной почты"
+                    {...register('email', {
+                      required : 'Введите email',
+                      pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'Некорректный адрес электронной почты'
+                      }
+                    })}
+                  />
+                  {errors.email && <span style={{color: '#f2890f'}}>{errors.email.message}</span>}
                 </div>
                 <div className="custom-input login-form__input">
                   <label className="custom-input__label" htmlFor="password">Пароль</label>
-                  <input type="password" id="password" name="password" placeholder="Пароль" required />
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="Пароль"
+                    {...register('password', {
+                      required: 'Введите пароль',
+                      minLength: {value: 3, message: 'Минимум 3 символа'},
+                      maxLength: {value: 15, message: 'Максимум 15 символов'},
+                      pattern: {value: /(?=.*[A-Za-zА-Яа-яЁё])(?=.*\d)/, message: 'Пароль должен содержать минимум одну букву и одну цифру'}
+                    })}
+                  />
+                  {errors.password && <span style={{color: '#f2890f'}}>{errors.password.message}</span>}
                 </div>
               </div>
-              <button className="btn btn--accent btn--general login-form__submit" type="submit">Войти</button>
+              {loginError && <span style={{color: '#f2890f'}}>{loginError}</span>}
+              <button className="btn btn--accent btn--general login-form__submit" type="submit" disabled={isSubmitting}>{isSubmitting ? 'Вход...' : 'Войти'}</button>
+              {errors['user-agreement'] && <span style={{color: '#f2890f', margin: 'auto'}}>{errors['user-agreement'].message}</span>}
             </div>
             <label className="custom-checkbox login-form__checkbox">
-              <input type="checkbox" id="id-order-agreement" name="user-agreement" required />
+              <input
+                type="checkbox"
+                id="id-order-agreement"
+                {...register('user-agreement', {required: 'Необходимо подтвердить согласие'})}
+              />
               <span className="custom-checkbox__icon">
                 <svg width="20" height="17" aria-hidden="true">
                   <use xlinkHref="#icon-tick"></use>
